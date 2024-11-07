c9efed0a2c4756d738c0ed8a5e829463f996a6b9
rajneesh.saini <rajneesh.saini@motional.com>
2024-11-07 14:03:45 -0800

    added async_std code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use async_std::io;
+use async_std::net::{TcpListener, TcpStream, Shutdown};
+use async_std::prelude::*;
+use async_std::task;
+
+async fn process_connection(stream: TcpStream) -> io::Result<()> {
+    let mut reader = stream.clone();
+    let mut writer = stream;
+    let mut buf = Vec::with_capacity(1500);
+    let n = reader.read(&mut buf).await?;
+    if n <= 0 {
+        // close socket
+    }
+    // else we received something, lets write back
+    println!("received {n} bytes on this tcp socket");
+    println!("-> {}\n", String::from_utf8_lossy(&buf[..n]));
+    // // htonl this
+    let _ = writer.write_all(format!("{}{}", 0 as u32, 7 as u32).as_bytes()).await?;
+    let _ = writer.shutdown(Shutdown::Both);
+    Ok(())
+
+}
+
+async fn process_tcp() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:9092").await?;
+    println!("Listening on {}", listener.local_addr()?);
+    let mut incoming = listener.incoming();
+    
+     while let Some(stream) = incoming.next().await {
+        let stream = stream?;
+        task::spawn(async {
+            process_connection(stream).await.unwrap();
+        });
+     }
+    Ok(())
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    
-    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    
-     for stream in listener.incoming() {
-         match stream {
-             Ok(_stream) => {
-                 println!("accepted new connection");
-             }
-             Err(e) => {
-                 println!("error: {}", e);
-             }
-         }
-     }
+    task::block_on(async {
+            let _ = process_tcp().await;
+        });
 }
