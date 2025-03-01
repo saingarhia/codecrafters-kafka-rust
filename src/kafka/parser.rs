891439f18eae215fe3e144260abe2ea5bd1e05ed
rajneesh.saini <rajneesh.saini@motional.com>
2025-02-28 23:33:07 -0800

    add missing files

---
+++ src/kafka/parser.rs
@@ -0,0 +1,45 @@
+// parsing utilities
+use std::io::{BufReader, Read};
+use crate::kafka::errors;
+
+pub fn compact_string(req: &mut BufReader<&[u8]>) -> errors::Result<Vec<u8>> {
+    let mut length = [0_u8; 1];
+    req.read_exact(&mut length)?;
+    // read these many bytes and convert into string
+    let mut data = vec![0_u8; length[0] as usize];
+    req.read_exact(&mut data)?;
+    Ok(data)
+}
+
+pub fn array(req: &mut BufReader<&[u8]>) -> errors::Result<Vec<Vec<u8>>> {
+    let mut length = [0_u8; 1];
+    req.read_exact(&mut length)?;
+    let mut result = vec![vec![]];
+    for _i in 0..length[0]{
+        result.push(compact_string(req)?);
+        tag_buffer(req)?;
+    }
+    Ok(result)
+}
+
+pub fn read_int(req: &mut BufReader<&[u8]>) -> errors::Result<i32> {
+    let mut data = [0_u8; 4];
+    req.read_exact(&mut data)?;
+    Ok(i32::from_be_bytes(data))
+}
+
+pub fn read_short(req: &mut BufReader<&[u8]>) -> errors::Result<i16> {
+    let mut data = [0_u8; 2];
+    req.read_exact(&mut data)?;
+    Ok(i16::from_be_bytes(data))
+}
+
+pub fn read_byte(req: &mut BufReader<&[u8]>) -> errors::Result<i8> {
+    let mut data = [0_u8; 1];
+    req.read_exact(&mut data)?;
+    Ok(data[0] as i8)
+}
+pub fn tag_buffer(req: &mut BufReader<&[u8]>) -> errors::Result<()> {
+    let _ = read_byte(req)?;
+    Ok(())
+}
---
+++ src/kafka/partitions.rs
@@ -0,0 +1,34 @@
+use std::io::{BufReader, Read};
+use crate::kafka::{parser, errors};
+
+#[derive(Debug, Clone)]
+pub struct Cursor {
+    pub topic_name: Vec<u8>,
+    pub partition_index: i32,
+}
+
+#[derive(Debug, Clone)]
+pub struct Partitions {
+    pub topics: Vec<Vec<u8>>,
+    pub response_partition_limit: i32,
+    pub cursor: Cursor, 
+}
+
+impl Partitions {
+    pub fn new(req: &mut BufReader<&[u8]>) -> errors::Result<Self> {
+            // read the length and then process the array
+        let topics = parser::array(req)?;
+        let response_partition_limit = parser::read_int(req)?;
+        let topic_name = parser::compact_string(req)?;
+        let partition_index = parser::read_int(req)?;
+        parser::tag_buffer(req)?;
+        Ok(Self {
+            topics,
+            response_partition_limit,
+            cursor: Cursor {
+                topic_name,
+                partition_index,
+            }
+        })
+    }
+}
