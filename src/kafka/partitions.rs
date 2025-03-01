4d7dc0d2d2c151051cd0d5f08a57961797f16e3a
rajneesh.saini <rajneesh.saini@motional.com>
2025-03-01 00:09:35 -0800

    deduct 1 from number of topics

--- src/kafka/partitions.rs
+++ src/kafka/partitions.rs
@@ -18,14 +18,18 @@ impl Partitions {
     pub fn new(req: &mut BufReader<&[u8]>) -> errors::Result<Self> {
             // read the length and then process the array
         let topics = parser::array(req)?;
-        println!("Found topics!!");
+        println!("Found {} topics!!", topics.len());
         for t in &topics {
             println!("topic name: {}", String::from_utf8_lossy(t));
         }
         let response_partition_limit = parser::read_int(req)?;
+        println!("response partition limit: {}", response_partition_limit);
         let topic_name = parser::compact_string(req)?;
+        println!("topic name: {}", String::from_utf8_lossy(&topic_name));
         let partition_index = parser::read_int(req)?;
+        println!("partition index: {}", partition_index);
         parser::tag_buffer(req)?;
+        println!("Now building final parition structure!!");
         Ok(Self {
             topics,
             response_partition_limit,
